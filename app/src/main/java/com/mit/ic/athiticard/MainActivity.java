9473deeb7b64cf315fd1a5257cf49959434be065
Rakshit GL <rakshitgl@live.com>
2019-02-24 09:39:40 +0530

    UI Upgrade

--- app/src/main/java/com/mit/ic/athiticard/MainActivity.java
+++ app/src/main/java/com/mit/ic/athiticard/MainActivity.java
@@ -1,7 +1,13 @@
 package com.mit.ic.athiticard;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -14,15 +20,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Fragment fragment;
-//        FragmentManager fm = getSupportFragmentManager();
-//        FragmentTransaction ft = fm.beginTransaction();
-//        fragment = new EncDetails();
-//        ft.replace(R.id.initAct,fragment);
-//        ft.commit();
     }
 
-    public void changeFragment(View view){
+    /*public void changeFragment(View view){
 
         Fragment fragment;
         FragmentManager fm = getSupportFragmentManager();
@@ -47,5 +47,25 @@ public class MainActivity extends AppCompatActivity {
         if(view == findViewById(R.id.jobButton)){
 
         }
-    }
+    }*/
+
+    /*private static final Pattern KEYCODE_PATTERN = Pattern.compile("/^[0-9]{10}$/");
+    private com.google.android.material.button.MaterialButton viewNumber;
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        viewNumber = findViewById(R.id.tapButton);
+        String cardNumber;
+
+        String key = KeyEvent.keyCodeToString(keyCode);
+        Matcher matcher = KEYCODE_PATTERN.matcher(key);
+        if (matcher.matches()) {
+            viewNumber.append(matcher.group(1));
+        }
+
+        viewNumber.setVisibility(View.VISIBLE);
+
+        return super.onKeyDown(keyCode, event);
+    }*/
 }
